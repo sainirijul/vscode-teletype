48023ee6eaaf3bcdf1ec27d625fe4c72173e6331
Rijul Saini <rijulsaini.mait@gmail.com>
2019-09-03 11:44:46 -0400

    removing the auth token

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 const API_URL_BASE = 'https://api.teletype.atom.io';
 const PUSHER_KEY = 'f119821248b7429bece3';
 const PUSHER_CLUSTER = 'mt1';
-const AUTH_TOKEN = '8caeb203e6d2c5c711a849477f778c6f1c628a69';
+const AUTH_TOKEN = '';
 
 module.exports = {
     API_URL_BASE,
