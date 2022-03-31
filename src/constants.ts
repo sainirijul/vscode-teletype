4b8474d062dda5a7ef1320383a97d83bf51f753c
Ju, Gyeong-min <jcmh74@gmail.com>
2022-03-31 12:50:09 +0900

    서버 포트 변경
    Signed-off-by: Ju, Gyeong-min <jcmh74@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@
 //export const API_URL_BASE: string = 'https://api.teletype.atom.io';
 //export const PUSHER_KEY: string = 'f119821248b7429bece3';
 //export const PUSHER_CLUSTER: string = 'mt1';
-export const API_URL_BASE: string = 'http://localhost:3000';
+export const API_URL_BASE: string = 'http://localhost:4279';
 export const PUSHER_KEY: string = 'cc8fb96b89e84d763d6b';
 //export const PUSHER_KEY: string = 'PUSHER_KEY=1234567890
 export const PUSHER_SECRET: string = '1882b79e0d1e521226d7';
